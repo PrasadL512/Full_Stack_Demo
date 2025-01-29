0d3635d4da1f4073d14e8f403740b08fc4e4c853
prasad Shivaji Lalge <prasad Shivaji Lalge@LAPTOP-178N5J62>
2025-01-29 16:32:55 +0530

    Add project files.

---
+++ Full_Stack_Demo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Full_Stack_Demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
