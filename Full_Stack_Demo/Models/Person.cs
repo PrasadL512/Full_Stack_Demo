05d02d396902d768b0efdd1762783291b0405747
prasad Shivaji Lalge <prasad Shivaji Lalge@LAPTOP-178N5J62>
2025-01-30 18:40:01 +0530

    Added Person model and created new controller Person

---
+++ Full_Stack_Demo/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Full_Stack_Demo.Models
+{
+    public class Person
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
